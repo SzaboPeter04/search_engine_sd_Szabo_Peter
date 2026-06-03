0d9907c1b9fc6d0b25bef88e6e96df4928ebb818
SzaboPeter04 <Szabo.La.Peter@student.utcluj.ro>
2026-06-03 16:02:46 +0300

    iteration3
    Added support for additional file types in indexing.

--- config.py
+++ config.py
@@ -1,20 +1,8 @@
-"""Simple configuration for the search engine.
-
-Change these values if your database or preferences differ.
-"""
-
-# Database connection (Postgres). Keep as-is for the demo.
 DB_HOST = "localhost"
 DB_PORT = 5432
 DB_NAME = "search_engine"
 DB_USER = "postgres"
 DB_PASSWORD = "12345"
-
-# File types we support indexing (lowercase suffixes)
-SUPPORTED_EXTENSIONS = {".txt", ".docx"}
-
-# How many characters to keep in the preview
+SUPPORTED_EXTENSIONS = {".txt", ".docx", ".pdf", ".log", ".png", ".jpg", ".jpeg"}
 PREVIEW_MAX_CHARS = 500
-
-# Max number of search results to return
 SEARCH_LIMIT = 50
